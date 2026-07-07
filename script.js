e90259026aeff832ec74334de6ad381a5e0b4b52
7997501646 <mahithjujjuri9@gmail.com>
2026-07-07 21:02:35 +0530

    feat: add habit progress

---
+++ script.js
@@ -0,0 +1,17 @@
+function saveHabit(){
+
+let habit=document.getElementById("habit").value;
+
+if(habit==""){
+
+alert("Enter a habit");
+
+return;
+
+}
+
+document.getElementById("result").innerHTML=
+
+"Habit Saved : "+habit;
+
+}
